a7bce97fd890800b5b1d4752bd969494a20f92be
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-07-05 12:53:05 +0530

    app setup
    adding cross env npm package.
    adding course add with and without answers

--- test/bar.test.js
+++ test/bar.test.js
@@ -1,23 +1,24 @@
 const assert = require('assert');
 describe('file u are testing', () => {
   context('function to be tested', () => {
-    before(() => {
-      console.log('=============== before');
-    });
+    // before(() => {
+    //   console.log('=============== before');
+    // });
 
-    after(() => {
-      console.log('============== after');
-    });
+    // after(() => {
+    //   console.log('============== after');
+    // });
 
-    beforeEach(() => {
-      console.log('=============== beforeEach');
-    });
+    // beforeEach(() => {
+    //   console.log('=============== beforeEach');
+    // });
 
-    afterEach(() => {
-      console.log('============== afterEach');
-    });
+    // afterEach(() => {
+    //   console.log('============== afterEach');
+    // });
     it('should do something', () => {
       assert.equal(1, 1);
+      console.log("ENV",process.env.NODE_ENV)
     });
 
     it('should do something else', () => {
